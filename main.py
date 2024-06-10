5e01701d51e26a4bc7c4fdcc48c6c71316c56834
Aaron DV <116379923+AaronDV17@users.noreply.github.com>
2024-06-10 15:33:53 +0200

    Updated main with stock & news API's, setup twilio Client to send SMS alert

--- main.py
+++ main.py
@@ -1,24 +1,80 @@
+import requests
+from dotenv import load_dotenv
+import os
+import datetime as dt
+from twilio.rest import Client
+
+load_dotenv()
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Datetime config
+# TODO: Uncomment old 'now' variable & delete temporary new 'now'
+# now = dt.datetime.now()
+now = dt.datetime(2024, 6, 8)
+yesterday = now - dt.timedelta(days=1)
+yesterday_str = yesterday.strftime('%Y-%m-%d')
+yesterday_minus_1 = now - dt.timedelta(days=2)
+yesterday_minus_1_str = yesterday_minus_1.strftime('%Y-%m-%d')
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Alpha Vantage API Details
+av_api_key = os.environ['AV_API_KEY']
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+av_url = 'https://www.alphavantage.co/query'
+av_params = {
+    'function': 'TIME_SERIES_DAILY',
+    'symbol': STOCK,
+    'apikey': av_api_key,
+}
 
+# News API Details
+news_api_key = os.environ['NEWS_API_KEY']
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+news_url = "https://newsapi.org/v2/everything"
+news_params = {
+    'q': COMPANY_NAME,
+    'from': yesterday_str,
+    'pageSize': 3,
+    'apiKey': news_api_key,
+}
 
+# Twilio Client Details
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+txt_out_no = os.environ['TXT_OUT']
+txt_in_no = os.environ['TXT_IN']
+
+# TODO: Update documentation
+# When STOCK price increase/decreases by 5% between yesterday & the day before yesterday
+av_r = requests.get(av_url, params=av_params)
+av_r.raise_for_status()
+stock_data = av_r.json()
+
+# TODO: Replace hardcoded price close values with values from 'stock_data'
+# yesterday_close = float(stock_data['Time Series (Daily)'][f'{yesterday_str}']['4. close'])
+# yesterday_minus_1_close = float(stock_data['Time Series (Daily)'][f'{yesterday_minus_1_str}']['4. close'])
+yesterday_close = 177.4800
+yesterday_minus_1_close = 177.9400
+price_delta = round(((yesterday_close / yesterday_minus_1_close) - 1) * 100, 2)
+
+if abs(price_delta) >= 5:
+    news_r = requests.get(news_url, params=news_params)
+    news_r.raise_for_status()
+    news_data = news_r.json()
+    headline = news_data['articles'][0]['title']
+    brief = news_data['articles'][0]['description']
+
+# Send a SMS with the percentage change & each article's title and description to your phone number.
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body=f"""
+                TSLA: {price_delta}%
+                Headline: {headline} 
+                Brief: {brief}
+                """,
+            from_=txt_out_no,
+            to=txt_in_no,
+        )
+    print(message.status)
